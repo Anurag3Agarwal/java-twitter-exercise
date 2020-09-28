e5d0f22cbc479daf09fc5771ba69c9ce1adcf2ff
Anurag Agarwal <anurag3.agarwal@gmail.com>
2020-09-28 15:02:41 +0200

    Adding a frontend placeholder to show tweets

--- src/main/java/com/ing/javaexercise/controller/TwitterClientController.java
+++ src/main/java/com/ing/javaexercise/controller/TwitterClientController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
@@ -24,13 +23,12 @@ public class TwitterClientController {
   private String searchString;
 
 
-
   @RequestMapping("tweets")
   public String fetchTweets(Model model) {
-    List<Tweet> tweets =  twitterStreamService.retrieveAndProcessTweets(searchString);
+    List<Tweet> tweets = twitterStreamService.retrieveAndProcessTweets(searchString);
     model.addAttribute("tweets", tweets);
     model.addAttribute("searchString", searchString);
-        System.out.println("Controller called");
+    System.out.println("Controller called");
     return "tweets";
   }
 
