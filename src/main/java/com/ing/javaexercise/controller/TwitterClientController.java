1cbe84c7ec0a2a9bc9f61c5e2a6a94f1e2d9b63f
Anurag Agarwal <anurag3.agarwal@gmail.com>
2020-09-28 15:40:04 +0200

    Adding security configuration

--- src/main/java/com/ing/javaexercise/controller/TwitterClientController.java
+++ src/main/java/com/ing/javaexercise/controller/TwitterClientController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -23,7 +24,7 @@ public class TwitterClientController {
   private String searchString;
 
 
-  @RequestMapping("tweets")
+  @GetMapping("tweets")
   public String fetchTweets(Model model) {
     List<Tweet> tweets = twitterStreamService.retrieveAndProcessTweets(searchString);
     model.addAttribute("tweets", tweets);
